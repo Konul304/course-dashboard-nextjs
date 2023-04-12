575266d5933d465c4b0b577b406989c706946a8f
Konul304 <memmedovakonul270@gmail.com>
2023-04-12 17:40:40 +0400

    add dot to tabs

--- course-dashboard/app/menu.js
+++ course-dashboard/app/menu.js
@@ -1,18 +1,22 @@
+'use client';
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCommentDots, faFolder, faGear, faHouse, faLayerGroup } from '@fortawesome/free-solid-svg-icons'
+import { faCircle, faCommentDots, faFolder, faGear, faHouse, faLayerGroup } from '@fortawesome/free-solid-svg-icons'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import Link from 'next/link';
 config.autoAddCss = false
+import { useRouter } from 'next/navigation';
 
 
 const Menu = () => {
+    const router = useRouter();
     return (
         <div className='menu'>
             <Link href='/overview' className='d-flex align-items-baseline'>
                 <FontAwesomeIcon icon={faHouse} className='icon' />
                 <p>Overview</p>
+                {router.pathname === '/overview' && <FontAwesomeIcon icon={faCircle} className='dot ms-4' size='xs' />}
             </Link>
             <Link href='/overview' className='d-flex align-items-baseline'>
                 <FontAwesomeIcon icon={faLayerGroup} className='icon' />
