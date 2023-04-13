731f9b276da3b03312d4de7ad6f964831e8a9813
Konul304 <memmedovakonul270@gmail.com>
2023-04-13 16:36:56 +0400

    fix layout

--- course-dashboard/app/page.js
+++ course-dashboard/app/page.js
@@ -1,13 +1,16 @@
 import { Inter } from 'next/font/google'
 import Layout from './layout2'
 import 'bootstrap/dist/css/bootstrap.css'
+import Overview from './overview/page'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <Layout/>
+      <Layout>
+        <Overview />
+      </Layout>
     </>
   )
 }
