6888252d25f208f1e38a97dad2176927e37edcd2
Konul304 <memmedovakonul270@gmail.com>
2023-04-13 16:38:40 +0400

    style overview component's heading button and input

--- course-dashboard/app/overview/page.js
+++ course-dashboard/app/overview/page.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import "../css/overview.css"
+import Link from 'next/link';
+import Image from 'next/image';
+import search from "../../public/images/search.svg"
+import MyCourses from '../components/myCourses';
 
 const Overview = () => {
+
+
     return (
         <>
+            <div className='overview_container'>
+                <p>Hello <b>BRUNO</b>, welcome back!</p>
+                <div className='d-flex justify-content-between align-items-baseline'>
+                    <div className='heading d-flex align-items-baseline justify-content-between'>
+                        <p className='me-4'>My Courses</p>
+                        <Link href={'/overview'}>View All</Link>
+                    </div>
+                    <div className='d-flex'>
+                        <Image 
+                        src={search}
+                        alt="search"
+                        />
+                        <input className='ms-3' type="text" placeholder='Search...' />
+                    </div>
+                </div>
+                <MyCourses/>
+            </div>
         </>
     );
 }
