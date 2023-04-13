fdece06ec412ca6008a993d1802298d649b3320f
Konul304 <memmedovakonul270@gmail.com>
2023-04-13 10:05:58 +0400

    add profile box

--- course-dashboard/app/rightMenu.js
+++ course-dashboard/app/rightMenu.js
@@ -2,6 +2,8 @@ import React from 'react';
 import "./css/rightMenu.css"
 import Image from 'next/image';
 import notif from '../public/images/notification.svg'
+import profile from '../public/images/profile.svg'
+import dropdown from '../public/images/dropdown.svg'
 import Statistics from './components/statistics';
 import Activity from './components/activity';
 
@@ -10,13 +12,31 @@ const RightMenu = () => {
     return (
         <>
             <div className='container_main'>
-                <div className='d-flex'>
+                <div className='d-flex justify-content-between align-items-center'>
                     <Image className=''
                         src={notif}
                         alt="Picture of the author"
                         width="25px"
                         height="26px"
                     />
+                    <div className='profileDiv d-flex align-items-center'>
+                        <Image className=''
+                            src={profile}
+                            alt="Picture of the author"
+                            width="40px"
+                            height="40px"
+                        />
+                        <div className='d-flex flex-column textDiv'>
+                            <span className='name'>Bruno Fernandes</span>
+                            <span className='plan'>Basic Plan</span>
+                        </div>
+                        <Image className='drop'
+                            src={dropdown}
+                            alt="Picture of the author"
+                            width="14px"
+                            height="8px"
+                        />
+                    </div>
                 </div>
                 <Statistics />
                 <Activity />
