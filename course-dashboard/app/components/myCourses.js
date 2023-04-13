11082d18273d9a76040c431ae7d14516a53364d7
Konul304 <memmedovakonul270@gmail.com>
2023-04-13 17:31:34 +0400

    create flex boxes

--- course-dashboard/app/components/myCourses.js
+++ course-dashboard/app/components/myCourses.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import "../css/myCourses.css"
 
 const MyCourses = () => {
     return (
         <>
-            
+            <div className='mt-3'>
+                <div className='d-flex '>
+                    <div className='flex flex1'></div>
+                    <div className='flex flex2'></div>
+                </div>
+                <div className='d-flex'>
+                    <div className='flex flex3'></div>
+                    <div className='flex flex4'></div>
+                </div>
+            </div>
         </>
     );
 }
