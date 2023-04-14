594f6509635021f686a0d7ab3aae705526d880a8
Konul304 <memmedovakonul270@gmail.com>
2023-04-14 10:51:12 +0400

    add box images and texts

--- course-dashboard/app/components/myCourses.js
+++ course-dashboard/app/components/myCourses.js
@@ -9,6 +9,10 @@ import bottom1 from "../../public/images/bottom1.svg"
 import bottom2 from "../../public/images/bottom2.svg"
 import bottom3 from "../../public/images/bottom3.svg"
 import bottom4 from "../../public/images/bottom4.svg"
+import paris from "../../public/images/paris.svg"
+import portugese from "../../public/images/portugese.svg"
+import italian from "../../public/images/italian.svg"
+import german from "../../public/images/german.svg"
 
 const MyCourses = () => {
     return (
@@ -18,20 +22,34 @@ const MyCourses = () => {
                     <div className='flex flex1'>
                         <Image className='top1' src={top1} alt='topShape' />
                         <Image className='bottom1' src={bottom1} alt='topShape' />
+                        <p>French</p>
+                        <span>35 lessons</span>
+
+
+                        <Image className='paris' src={paris} alt='topShape' />
                     </div>
                     <div className='flex flex2'>
                         <Image className='top2 ' src={top2} alt='topShape' />
                         <Image className='bottom2' src={bottom2} alt='topShape' />
+                        <p>Portugese</p>
+                        <span>30 lessons</span>
+                        <Image className='portugese' src={portugese} alt='topShape' />
                     </div>
                 </div>
                 <div className='d-flex'>
                     <div className='flex flex3'>
                         <Image className='top3' src={top3} alt='topShape' />
                         <Image className='bottom3' src={bottom3} alt='topShape' />
+                        <p>Italian</p>
+                        <span>20 lessons</span>
+                        <Image className='italian' src={italian} alt='topShape' />
                     </div>
                     <div className='flex flex4'>
                         <Image className='top4' src={top4} alt='topShape' />
                         <Image className='bottom4' src={bottom4} alt='topShape' />
+                        <p>German</p>
+                        <span>40 lessons</span>
+                        <Image className='german' src={german} alt='topShape' />
                     </div>
                 </div>
             </div>
